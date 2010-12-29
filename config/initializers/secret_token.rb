e0da8a5a9bfe4ba23fb26b251c9c03951c9a6c49
Rafael Fernández López <ereslibre@ereslibre.es>
2010-12-29 23:20:15 +0100

    Initial import of Luvs project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Luvs::Application.config.secret_token = '405277a3b94865a3492dd6f55d9bbcf9a07a29c32a317bbf28b0b1eddda8e46767f1e5d17d0865e17627ce2627a05c318c1f4ef5e2847b7bb0104b3462b037ff'
